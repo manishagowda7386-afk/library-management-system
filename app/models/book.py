7f4371c7da4a6d98881379b46233df99cf71ee04
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-18 12:29:05 +0530

    Implement Book model

--- app/models/book.py
+++ app/models/book.py
@@ -1,1 +1,20 @@
-
+class Book:
+    def __init__(
+        self,
+        book_id,
+        title,
+        author,
+        isbn,
+        category,
+        publication_year,
+        total_copies,
+        available_copies,
+    ):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.category = category
+        self.publication_year = publication_year
+        self.total_copies = total_copies
+        self.available_copies = available_copies
