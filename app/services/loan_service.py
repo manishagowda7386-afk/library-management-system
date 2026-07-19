0c04866aa54960c1e349f5b5cea968ff56377d4c
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-19 12:56:27 +0000

    Implement book issue and return service

--- app/services/loan_service.py
+++ app/services/loan_service.py
@@ -1,21 +1,52 @@
 from app.repositories.loan_repository import LoanRepository
+from app.models.loan import Loan
 
 
 class LoanService:
     def __init__(self):
         self.loan_repository = LoanRepository()
 
-    def add_loan(self, loan):
+    def issue_book(
+        self,
+        loan_id,
+        book,
+        member,
+        issue_date,
+        due_date,
+    ):
+        if book.available_copies <= 0:
+            return False
+
+        loan = Loan(
+            loan_id,
+            book.book_id,
+            member.member_id,
+            issue_date,
+            due_date,
+        )
+
         self.loan_repository.add_loan(loan)
+
+        book.available_copies -= 1
+
+        return True
+
+    def return_book(self, loan_id, book):
+        loan = self.loan_repository.find_loan_by_id(loan_id)
+
+        if loan is None:
+            return False
+
+        if loan.returned:
+            return False
+
+        loan.returned = True
+        book.available_copies += 1
+
+        return True
 
     def get_all_loans(self):
         return self.loan_repository.get_all_loans()
 
     def find_loan_by_id(self, loan_id):
         return self.loan_repository.find_loan_by_id(loan_id)
-
-    def remove_loan(self, loan_id):
-        return self.loan_repository.remove_loan(loan_id)
-
-    def update_loan(self, loan_id, updated_loan):
-        return self.loan_repository.update_loan(loan_id, updated_loan)
