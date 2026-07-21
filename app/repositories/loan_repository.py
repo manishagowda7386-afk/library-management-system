e59805eac019c954ead3bccbba993d6ab0dcd4b0
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-21 04:03:31 +0000

    Persist loans using JSON

--- app/repositories/loan_repository.py
+++ app/repositories/loan_repository.py
@@ -1,9 +1,15 @@
+from app.utils.json_handler import JsonHandler
+
+
 class LoanRepository:
+    FILE_PATH = "data/loans.json"
+
     def __init__(self):
-        self.loans = []
+        self.loans = JsonHandler.load_data(self.FILE_PATH)
 
     def add_loan(self, loan):
-        self.loans.append(loan)
+        self.loans.append(loan.__dict__)
+        JsonHandler.save_data(self.FILE_PATH, self.loans)
 
     def get_all_loans(self):
         return self.loans
@@ -11,9 +17,9 @@ class LoanRepository:
     def find_loan(self, member_id, book_id):
         for loan in self.loans:
             if (
-                loan.member_id == member_id
-                and loan.book_id == book_id
-                and not loan.returned
+                loan["member_id"] == member_id
+                and loan["book_id"] == book_id
+                and not loan["returned"]
             ):
                 return loan
         return None
@@ -23,15 +29,10 @@ class LoanRepository:
 
         if loan:
             self.loans.remove(loan)
+            JsonHandler.save_data(self.FILE_PATH, self.loans)
             return True
 
         return False
 
-    def get_member_loans(self, member_id):
-        member_loans = []
-
-        for loan in self.loans:
-            if loan.member_id == member_id:
-                member_loans.append(loan)
-
-        return member_loans
+    def update_loans(self):
+        JsonHandler.save_data(self.FILE_PATH, self.loans)
