d6becdbac61d85ba0c9b5b103a0ae57d64f72501
Maneesha Tech <manisha.gowda7386@gmail.com>
2026-07-21 04:06:55 +0000

    Add serialization support to Book model

--- app/models/book.py
+++ app/models/book.py
@@ -19,13 +19,35 @@ class Book:
         self.total_copies = total_copies
         self.available_copies = available_copies
 
+    def to_dict(self):
+        return {
+            "book_id": self.book_id,
+            "title": self.title,
+            "author": self.author,
+            "isbn": self.isbn,
+            "category": self.category,
+            "publication_year": self.publication_year,
+            "total_copies": self.total_copies,
+            "available_copies": self.available_copies,
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            data["book_id"],
+            data["title"],
+            data["author"],
+            data["isbn"],
+            data["category"],
+            data["publication_year"],
+            data["total_copies"],
+            data["available_copies"],
+        )
+
     def __str__(self):
         return (
-            f"{self.book_id} | "
-            f"{self.title} | "
-            f"{self.author} | "
-            f"ISBN: {self.isbn} | "
-            f"Category: {self.category} | "
-            f"Published: {self.publication_year} | "
+            f"ID: {self.book_id} | "
+            f"Title: {self.title} | "
+            f"Author: {self.author} | "
             f"Available: {self.available_copies}/{self.total_copies}"
-        ) 
+        )
